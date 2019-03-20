3e88fc58bfa480d3afd8d6824abce0c856cb7c13
James Thomas <james@jamesthom.as>
2019-03-20 17:22:57 +0000

    Adding frontend app

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 const releases = require('./lib/releases.js')
 const verify = require('./lib/verify.js')
-const archive = require('./lib/archive.js')
 const fs = require('fs')
 
+// NEED TO WORK ON ERRORS
 ;(async ()=>{
   const project = 'openwhisk'
   const versions = await releases.versions(project)
   console.log(versions)
 
-  const version = 'apache-openwhisk-3.19.0-incubating-rc1'
+  const version = 'apache-openwhisk-0.10.0-incubating-rc1'
   const files = await releases.files(project, version)
   console.log(files)
 
@@ -25,33 +25,16 @@ const fs = require('fs')
 
     const hash_operation = verify.hash(fs.createReadStream(file_name), hash)
     const sig_operation = verify.signature(fs.createReadStream(file_name), sig, keys)
+    const archive_operation = verify.archive_files(fs.createReadStream(file_name), sig, keys)
 
-    const [hash_result, sig_result] = await Promise.all([hash_operation, sig_operation])
+    const [hash_result, sig_result, archive_files] = await Promise.all([hash_operation, sig_operation, archive_operation])
 
-    const result = { name: file, is_sig_valid: sig_result.valid, is_hash_valid: hash_result.valid, is_disclaimer_valid: false, is_notice_valid: false, is_license_valid: false } 
-
-    const file_stream = fs.createReadStream(file_name)
-    try {
-      const contents = await archive.files(file_stream, ['DISCLAIMER.txt', 'NOTICE.txt', 'LICENSE.txt'])
-
-      if (contents['DISCLAIMER.txt']) {
-        result.is_disclaimer_valid = verify.disclaimer(contents['DISCLAIMER.txt'])
-      }
-      if (contents['NOTICE.txt']) {
-        result.is_notice_valid = verify.notice(contents['NOTICE.txt'])
-      }
-
-      if (contents['LICENSE.txt']) {
-        result.is_license_valid = verify.license(contents['LICENSE.txt'])
-      }
-
-    } catch (err)  {
-      console.log(err)
-    }
+    const result = { name: file, is_sig_valid: sig_result.valid, is_hash_valid: hash_result.valid, archive_files } 
 
     return result
   })
 
+  // what about errors?
   const result = await Promise.all(file_checks)
   console.log(result)
 })();
