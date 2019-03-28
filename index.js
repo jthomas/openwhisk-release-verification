ebb92dfaf7c3b73c01382f6739c85c1231e44a3e
James Thomas <james@jamesthom.as>
2019-03-28 11:25:36 +0000

    adding more acceptance tests

--- index.js
+++ index.js
@@ -19,21 +19,27 @@ const version_files = async params => {
 }
 
 const file_check = async (project, version, file, keys) => {
-  console.log('verifying', file)
-  const sig = await releases.file_sig(project, version, file)
-  const hash = await releases.file_hash(project, version, file)
+  try {
+    console.log('verifying', file)
+    const sig = await releases.file_sig(project, version, file)
+    const hash = await releases.file_hash(project, version, file)
 
-  const file_name = await releases.download_file(project, version, file)
+    const file_name = await releases.download_file(project, version, file)
 
-  const hash_operation = verify.hash(fs.createReadStream(file_name), hash)
-  const sig_operation = verify.signature(fs.createReadStream(file_name), sig, keys)
-  const archive_operation = verify.archive_files(fs.createReadStream(file_name), sig, keys)
+    const hash_operation = verify.hash(fs.createReadStream(file_name), hash)
+    const sig_operation = verify.signature(fs.createReadStream(file_name), sig, keys)
+    const archive_operation = verify.archive_files(fs.createReadStream(file_name), sig, keys)
 
-  const [hash_result, sig_result, archive_files] = await Promise.all([hash_operation, sig_operation, archive_operation])
+    const [hash_result, sig_result, archive_files] = await Promise.all([hash_operation, sig_operation, archive_operation])
 
-  const result = { name: file, files_valid: true, sig_valid: sig_result.valid, hash_valid: hash_result.valid, archive_files } 
+    const result = { name: file, files_valid: true, sig_valid: sig_result.valid, hash_valid: hash_result.valid, archive_files } 
 
-  return result
+    return result
+  } catch (err) {
+    // HTTP issues validating files should be returned as internal server errors
+    err.status = 500
+    throw err
+  }
 }
 
 const validate_version_file = async params => {
@@ -45,13 +51,25 @@ const validate_version_file = async params => {
   const files = await releases.files(PROJECT, version)
   console.log('checking the following files:', files)
 
-  // what about errors?
   const keys = await releases.keys(PROJECT)
   const file_checks = files.map(file => file_check(PROJECT, version, file, keys))
 
   const result = await Promise.all(file_checks)
- 
+
   return { body: { files: result } } 
 }
 
-module.exports = { versions, version_files, validate_version_file }
+const wrap_errors = handler => {
+  const wrapper = async params => {
+    try {
+      const result = await handler(params)
+      return result
+    } catch (err) {
+      return { statusCode: err.status || 500, body: { error: err.message } } 
+    }
+  }
+
+  return wrapper
+}
+
+module.exports = { versions, version_files: wrap_errors(version_files), validate_version_file: wrap_errors(validate_version_file) }
--- test/acceptance/api.js
+++ test/acceptance/api.js
@@ -17,6 +17,26 @@ test.serial('should return list of release candidates', async t => {
   t.deepEqual(result, { versions: state.versions })
 })
 
+test.serial('should return 404 for file list when release candidate is invalid', async t => {
+  const version = 'unknown'
+  const files_api = `https://${API_HOST}/api/versions/${version}`
+  t.log('running api testing against', files_api)
+  const resp = await fetch(files_api)
+  t.is(resp.status, 404)
+  const result = await resp.json()
+  t.deepEqual(result.error, 'release file at https://dist.apache.org/repos/dist/dev/incubator/openwhisk/unknown unavailable, status code: 404')
+})
+
+test.serial('should return 404 for validation when release candidate is invalid', async t => {
+  const version = 'unknown'
+  const files_api = `https://${API_HOST}/api/versions/${version}/validate`
+  t.log('running api testing against', files_api)
+  const resp = await fetch(files_api)
+  t.is(resp.status, 404)
+  const result = await resp.json()
+  t.deepEqual(result.error, 'release file at https://dist.apache.org/repos/dist/dev/incubator/openwhisk/unknown unavailable, status code: 404')
+})
+
 test.serial('should return file list of each release candidate', async t => {
   const reqs = state.versions.map(async version => {
     const files_api = `https://${API_HOST}/api/versions/${version}`
